8802d0fe69dd7c042fe4db3ff39baf5c15854af3
GrafanyS <admin@valet.of.by>
2022-12-23 20:44:49 +0300

    Count

---
+++ src/Lesson1/Counted.java
@@ -0,0 +1,24 @@
+package Lesson1;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+class Counted {
+    private static final AtomicInteger COUNTER = new AtomicInteger(1);
+
+    private final int id;
+
+    public Counted() {
+        id = COUNTER.getAndIncrement();
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+class  Main {
+    public static void main(String[] args) {
+        System.out.println(new Counted().getId()); //1
+        System.out.println(new Counted().getId()); //2
+        System.out.println(new Counted().getId()); //3
+    }
+}
