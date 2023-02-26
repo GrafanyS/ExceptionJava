ccaa56cdee8660ebedc0d833663918d80feb7eb7
GrafanyS <admin@valet.of.by>
2023-02-26 18:31:52 +0300

    Проверьте пол

--- src/Lesson3/base/classes/checkInputData/CCheckSex.java
+++ src/Lesson3/base/classes/checkInputData/CCheckSex.java
@@ -4,13 +4,13 @@ import Lesson3.base.abstractClasses.ACheckData;
 import Lesson3.base.exceptions.CheckSexException;
 import Lesson3.base.exceptions.IncorrectSexValuesException;
 
+// Проверьте пол
 public class CCheckSex extends ACheckData {
     @Override
-    public boolean checkElement(String data) {
+    public void checkElement(String data) {
         if(data.equals(""))
             throw new CheckSexException();
         if(!(data.contains("f") || data.contains("m")))
             throw new IncorrectSexValuesException();
-        return true;
     }
 }
