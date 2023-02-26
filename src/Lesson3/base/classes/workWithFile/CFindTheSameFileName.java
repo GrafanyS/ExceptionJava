69aba55d791e573ada71571da2966af1cbea09e9
GrafanyS <admin@valet.of.by>
2023-02-26 18:42:37 +0300

    класс находит то же имя файла

--- src/Lesson3/base/classes/workWithFile/CFindTheSameFileName.java
+++ src/Lesson3/base/classes/workWithFile/CFindTheSameFileName.java
@@ -1,9 +1,11 @@
 package Lesson3.base.classes.workWithFile;
 
 import java.io.File;
+import java.util.Objects;
 
 import Lesson3.base.abstractClasses.AFindTheSameFileName;
 
+// класс находит то же имя файла
 public class CFindTheSameFileName extends AFindTheSameFileName {
 
 
@@ -12,7 +14,7 @@ public class CFindTheSameFileName extends AFindTheSameFileName {
         super.folder = new File(folderName);
         boolean found = false;
 
-        for(File file: this.folder.listFiles()){
+        for(File file: Objects.requireNonNull(this.folder.listFiles())){
             String fileName = file.getName().split("\\.")[0];
 
             if(fileName.equals(lastName)) {
