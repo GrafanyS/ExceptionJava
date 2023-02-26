0cac7dee001fbf650c2ae46e2158fd853b135c37
GrafanyS <admin@valet.of.by>
2023-02-26 18:30:29 +0300

    Проверьте процессор обработки данных

--- src/Lesson3/base/classes/checkInputData/CCheckDataProcessor.java
+++ src/Lesson3/base/classes/checkInputData/CCheckDataProcessor.java
@@ -5,36 +5,33 @@ import Lesson3.base.abstractClasses.ACheckDataProcessor;
 import Lesson3.base.abstractClasses.ACheckFullName;
 import Lesson3.base.abstractClasses.ACheckQuantity;
 
+// Проверьте процессор обработки данных
 public class CCheckDataProcessor extends ACheckDataProcessor {
     public CCheckDataProcessor(ACheckQuantity checkQuantity, ACheckFullName checkFullName, ACheckData checkBirthday, ACheckData checkSex) {
         super(checkQuantity, checkFullName, checkBirthday, checkSex);
     }
 
     @Override
-    public boolean checkBirthday(String birthday) {
+    public void checkBirthday(String birthday) {
         super.checkBirthday.checkElement(birthday);
         System.out.println("Birthday format is OK.");
-        return true;
     }
 
     @Override
-    public boolean checkFullName(String[] fullName) {
+    public void checkFullName(String[] fullName) {
         super.checkFullName.checkFullName(fullName);
         System.out.println("Full name format is OK.");
-        return true;
     }
 
     @Override
-    public boolean checkQuantity(String[] data) {
+    public void checkQuantity(String[] data) {
         super.checkQuantity.checkQuantity(data);
         System.out.println("Quantity data is OK.");
-        return true;
     }
 
     @Override
-    public boolean checkSex(String sex) {
+    public void checkSex(String sex) {
         super.checkSex.checkElement(sex);
         System.out.println("Sex format is OK.");
-        return true;
     }
 }
