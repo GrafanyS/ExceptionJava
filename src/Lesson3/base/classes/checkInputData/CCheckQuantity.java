677340482bcbcc9db454fe9fa000ae46e648adcb
GrafanyS <admin@valet.of.by>
2023-02-26 18:31:27 +0300

    проверка количества

--- src/Lesson3/base/classes/checkInputData/CCheckQuantity.java
+++ src/Lesson3/base/classes/checkInputData/CCheckQuantity.java
@@ -3,11 +3,11 @@ package Lesson3.base.classes.checkInputData;
 import Lesson3.base.abstractClasses.ACheckQuantity;
 import Lesson3.base.exceptions.CheckQuantityException;
 
+// проверка количества
 public class CCheckQuantity extends ACheckQuantity {
     @Override
-    public boolean checkQuantity(String data[]) {
+    public void checkQuantity(String[] data) {
         if(data.length!=6)
             throw new CheckQuantityException();
-        return true;
     }
 }
