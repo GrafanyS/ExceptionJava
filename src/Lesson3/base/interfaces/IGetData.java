fec62a15cbf08693818e5155e2433e6c071ad8d9
GrafanyS <admin@valet.of.by>
2023-02-26 18:44:34 +0300

    интерфейс Getdata

--- src/Lesson3/base/interfaces/IGetData.java
+++ src/Lesson3/base/interfaces/IGetData.java
@@ -3,5 +3,5 @@ package Lesson3.base.interfaces;
 import java.io.IOException;
 
 public interface IGetData {
-    public String getData() throws IOException;
+    String getData() throws IOException;
 }
