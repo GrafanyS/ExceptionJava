f529aef05834cecfa08045a3e773f3a1bab6da8c
GrafanyS <admin@valet.of.by>
2023-02-26 18:53:35 +0300

    Класс создателя файла

--- src/Lesson3/base/classes/workWithFile/CFileCreator.java
+++ src/Lesson3/base/classes/workWithFile/CFileCreator.java
@@ -6,17 +6,19 @@ import java.io.IOException;
 import Lesson3.base.abstractClasses.AFileCreator;
 import Lesson3.base.exceptions.FileCreateException;
 
+// Класс создателя файла
+@SuppressWarnings("ALL")
 public class CFileCreator extends AFileCreator {
     /**
      * Создает новый файл по указанному пути.
+     *
      * @param path - путь плюс имя файла и расширение.
-     * @return - возвращает true в случае удачного создания.
      */
     @Override
-    public boolean createFile(String path) throws FileCreateException {
+    public void createFile(String path) throws FileCreateException {
         super.file = new File(path);
         try {
-            return super.file.createNewFile();
+            super.file.createNewFile();
         }catch (IOException e){
             throw new FileCreateException(e.getMessage());
         }
